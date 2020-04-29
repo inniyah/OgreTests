35da54a22829636267781d61226b668962dc340a
Miriam Ruiz <miriam@debian.org>
2020-04-29 23:01:08 +0200

    Restructured map tiles

---
+++ tiled/maps/tiles.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tiles" tilewidth="128" tileheight="224" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="3DModel" value="wall.obj"/>
+  </properties>
+  <image source="../models/wall_iso.png" width="128" height="224"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="3DModel" value="colmn.obj"/>
+  </properties>
+  <image source="../models/colmn_iso.png" width="128" height="224"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="3DModel" value="caution.obj"/>
+  </properties>
+  <image source="../models/caution_iso.png" width="128" height="224"/>
+ </tile>
+</tileset>
