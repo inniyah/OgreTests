44dc72cd3e6b728d9d902d0d826dc9fd1dd8e0f0
Miriam Ruiz <miriam@debian.org>
2020-04-29 22:33:13 +0200

    Adde 3DModel identifier to the tiles

--- tiled/maps/wall.tsx
+++ tiled/maps/wall.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="wall" tilewidth="128" tileheight="224" tilecount="1" columns="1">
+ <properties>
+  <property name="3DModel" value="wall.obj"/>
+ </properties>
  <image source="../models/wall_iso.png" width="128" height="224"/>
+ <tile id="0">
+  <properties>
+   <property name="3DModel" value="wall.obj"/>
+  </properties>
+ </tile>
 </tileset>
