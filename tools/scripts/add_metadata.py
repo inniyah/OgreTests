90a3b145a8ecb6d27688c50f125843364c21fa90
Miriam Ruiz <miriam@debian.org>
2020-05-24 18:29:11 +0200

    Using custom python script for columns and models

--- tools/scripts/add_metadata.py
+++ tools/scripts/add_metadata.py
@@ -10,6 +10,7 @@ from data_db import get_obj_info, update_obj_info
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process obj 3d models")
     parser.add_argument("objfile", help="path to an .obj")
+    parser.add_argument('--verbose', '-v', action='count', default=0)
     parser.add_argument("-d", "--db", help="path to the json db", default=None)
     parser.add_argument("-t", "--type", help="type of file", default=None)
     parser.add_argument("-J", "--json", help="JSON file with extra attributes", default=None)
@@ -24,9 +25,11 @@ if __name__ == "__main__":
 
     if args.D:
         for (key, value) in args.D:
+            if (args.verbose > 0): print(f"[{args.db}] @{key} = {value}")
             obj_info[f"@{key}"] = value
 
     if args.json:
+        if (args.verbose > 0): print(f"[{args.db}] :attrs_file = {args.json}")
         obj_info[f":attrs_file"] = args.json
 
     update_obj_info(args.db, args.objfile, obj_info)
